22271c9bb010a2bbeb27a4852eff8b0331325de9
Victim <victor.devv@gmail.com>
2022-07-17 15:10:09 -0300

    Add Invaders.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use crossterm::{
 use invaders::{
     frame::{self, new_frame, Drawable},
     player::Player,
-    render,
+    render, invaders::Invaders,
 };
 use rusty_audio::Audio;
 
@@ -58,6 +58,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Game Loop
     let mut player = Player::new();
     let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
 
     'gameloop: loop {
         // Per-frame Init
@@ -87,9 +88,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates Timers
         player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
 
         // Draw and Render
-        player.draw(&mut curr_frame);
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
+        
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
